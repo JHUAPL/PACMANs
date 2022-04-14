97fe27fa9c680b6bb3099bbd328bc63a55244131
Dr. Jay Brett <45367061+gjayb@users.noreply.github.com>
2022-04-14 08:58:34 -0400

    Update freshwaterfluxN.m

--- matlabCESM2/freshwaterfluxN.m
+++ matlabCESM2/freshwaterfluxN.m
@@ -1,47 +1,56 @@
-function [Fwn] = freshwaterfluxN(latNorth,depth,meanlatA,meanSA,meanlatVA,volumeVA,meanVA,dyu)
-%freshwaterflux Summary of this function goes here
-%   meanVA is zonally averaged northward velocity in m/s 
-%   volume and dyu in m^2 and m
-
-    if length(latNorth)==1
-        ni2=length(meanlatA(:,1));
-        niv=find(meanlatVA(:,1)>latNorth,1,'first');
-        ni=find(meanlatA(:,1)>meanlatVA(niv,1),1,'first');
-        ni2v=length(meanlatVA(:,1));
-    elseif length(latNorth)==2
-        niv=find(meanlatVA(:,1)>latNorth(1),1,'first');
-        ni=find(meanlatA(:,1)>meanlatVA(niv,1),1,'first');
-        ni2v=find(meanlatVA(:,1)<latNorth(2),1,'last');
-        ni2=find(meanlatA(:,1)>meanlatVA(ni2v,1),1,'first');
-    end
+% Copyright 2022, The Johns Hopkins University Applied Physics Laboratory LLC
+% All rights reserved.
+% Distributed under the terms of the BSD 3-Clause License.
+function [Fwn] = freshwaterfluxN(coordinates, depth, Atlantic, dyu)
+% freshwaterfluxN computes the freshwater flux from the northward salt flux
+% across coordinates.latNorth 1 and 2
+% using a depth-integral of Atlantic.salt and Atlantic.velN, which are
+% zonally averaged Atlantic values; expects volume and dyu in m^2 and m
 
 
-dedge=depth(1:end-1)+0.5*diff(depth); %depth_m_1 is the cell center; finding the edges to make dz
-dedge=cat(1,0,dedge,depth(end)+0.5*(depth(end)-depth(end-1)));
-dz=diff(dedge.');%m
-[~,nz,nt]=size(meanVA);
+niv = find(Atlantic.latV(:, 1) > coordinates.latNorth1, 1, 'first');
+ni = find(Atlantic.latT(:, 1) > Atlantic.latV(niv, 1), 1, 'first');
+ni2v = find(Atlantic.latV(:, 1) < coordinates.latNorth2, 1, 'last');
+ni2 = find(Atlantic.latT(:, 1) > Atlantic.latV(ni2v, 1), 1, 'first');
 
-%dz1(1,1:nz,1)=dz; dz1(isnan(meanVA(siv,:,1)))=0;
-dz1(1,1:nz,1)=dz; dz1(isnan(meanVA(niv,:,1)))=0;
-dz2(1,1:nz,1)=dz; dz2(isnan(meanVA(ni2v,:,1)))=0;
 
-vbar=zeros([2 1 nt]);
-vbar(1,1,:)=nansum(meanVA(niv,:,:).*repmat(dz1,[1 1 nt]),2)./sum(dz1);
-vbar(2,1,:)=nansum(meanVA(ni2v,:,:).*repmat(dz2,[1 1 nt]),2)./sum(dz2);
-vtransport=meanVA([niv,ni2v],:,:)-vbar; %relies on matlab 2016b or later to expand dimensions of vbar
+%depth is the cell center; finding the edges to make dz
+dedge = depth(1:end-1) + 0.5 * diff(depth);
+dedge = cat(1, 0, dedge, depth(end)+0.5*(depth(end) - depth(end-1)));
+dz = diff(dedge.'); %m
+[~, nz, nt] = size(Atlantic.velN);
 
-[~,~,nt]=size(vtransport);
-Fwn=zeros(nt,2);
+dz1(1, 1:nz, 1) = dz;
+dz1(isnan(Atlantic.velN(niv, :, 1))) = 0;
+dz2(1, 1:nz, 1) = dz;
+dz2(isnan(Atlantic.velN(ni2v, :, 1))) = 0;
+
+vbar = zeros([2, 1, nt]);
+vbar(1, 1, :) = nansum(Atlantic.velN(niv, :, :).*...
+    repmat(dz1, [1, 1, nt]), 2) ./ sum(dz1);
+vbar(2, 1, :) = nansum(Atlantic.velN(ni2v, :, :).*...
+    repmat(dz2, [1, 1, nt]), 2) ./ sum(dz2);
+vtransport = Atlantic.velN([niv, ni2v], :, :) - vbar; 
+%relies on matlab 2016b or later to expand dimensions of vbar
+
+[~, ~, nt] = size(vtransport);
+Fwn = zeros(nt, 2);
 
 %north 1
-    holdS=0.5*squeeze(meanSA(ni-1,:,:)+meanSA(ni,:,:));
-    holdS(isnan(holdS))=0;
-    Fwn(:,1)=-(1./35).*nansum(holdS.*squeeze(vtransport(1,:,:)).*repmat(squeeze(volumeVA(niv,:)).',[1 nt]),1)./nanmean(dyu(:,niv));%volume of each V cell divided by northward extent
+holdS = 0.5 * squeeze(Atlantic.salt(ni-1, :, :)+Atlantic.salt(ni, :, :));
+holdS(isnan(holdS)) = 0;
+Fwn(:, 1) = -(1 ./ 35) .* nansum(holdS.*squeeze(vtransport(1, :, :))...
+    .*repmat(squeeze(Atlantic.volumeV(niv, :)).', [1, nt]), 1) ./ ...
+    nanmean(dyu(:, niv));
+% integrating dxdz, so the volume of each V cell divided by northward
+% extent, dyu
 
 %north 2
-    holdS=0.5*squeeze(meanSA(ni2-1,:,:)+meanSA(ni2,:,:));
-    holdS(isnan(holdS))=0;
-    Fwn(:,2)=-(1./35).*nansum(holdS.*squeeze(vtransport(2,:,:)).*repmat(squeeze(volumeVA(ni2v,:)).',[1 nt]),1)./nanmean(dyu(:,ni2v));%volume of each V cell divided by northward extent
+holdS = 0.5 * squeeze(Atlantic.salt(ni2-1, :, :)+Atlantic.salt(ni2, :, :));
+holdS(isnan(holdS)) = 0;
+Fwn(:, 2) = -(1 ./ 35) .* nansum(holdS.*squeeze(vtransport(2, :, :)).*...
+    repmat(squeeze(Atlantic.volumeV(ni2v, :)).', [1, nt]), 1) ./ ...
+    nanmean(dyu(:, ni2v));
 
 
 end
